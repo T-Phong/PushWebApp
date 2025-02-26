60fa9f8eb293e64f838df184555669ff53314e03
T-Phong <T-Phong>
2025-02-26 13:06:38 +0700

    1

--- script.js
+++ script.js
@@ -28,12 +28,10 @@ window.addEventListener("beforeinstallprompt", (e) => {
 });
 window.OneSignal = window.OneSignal || [];
     OneSignal.push(function() {
-        OneSignal.config.serviceWorkerPath = "/PushWebApp/OneSignalSDKWorker.js";
         OneSignal.init({
             appId: "6fcc36e2-b2e6-4be9-bb54-0497bd56591d", // Thay YOUR_APP_ID bằng App ID từ OneSignal
             notifyButton: { enable: true }, // Hiển thị nút đăng ký thông báo
-            allowLocalhostAsSecureOrigin: true, // Cho phép chạy trên localhost
-            serviceWorkerPath: "/PushWebApp/OneSignalSDKWorker1.js"
+            serviceWorkerPath: "https://t-phong.github.io/PushWebApp/OneSignalSDKWorker.js"
         });
     });
 OneSignal.push(function() {
